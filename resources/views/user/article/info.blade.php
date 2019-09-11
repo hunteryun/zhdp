6b46f71ba49fcea3f9a19e1d6a9649ff52d4a289
xiaqiuchu <27804306+xiaqiuchu@users.noreply.github.com>
2019-09-11 11:06:41 +0800

    文章评论完成

--- resources/views/user/article/info.blade.php
+++ resources/views/user/article/info.blade.php
@@ -19,6 +19,19 @@
          </div>
      </div>
      <blockquote class="layui-elem-quote">
+         回复
+    </blockquote>
+    <div class="layui-form layui-form-pane">
+        <div class="layui-form-item layui-form-text">
+            <div class="layui-input-block">
+                <textarea type="text" class="layui-textarea" id="my-comment" name="my-comment" placeholder="评论内容"></textarea>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <button type="submit" id="submit" class="layui-btn" lay-submit="" lay-filter="formSubmit">提交</button>
+        </div>
+    </div>
+     <blockquote class="layui-elem-quote">
          评论
     </blockquote>
     <div id="article_comment">
@@ -50,6 +63,52 @@
                 }
             }
         });
+        // 初始化编辑器
+        var layedit = layui.layedit;
+        var layeditNode = layedit.build('my-comment',{
+            tool:[  
+                'strong' //加粗
+                ,'italic' //斜体
+                ,'underline' //下划线
+                ,'del' //删除线
+                ,'|' //分割线
+                ,'left' //左对齐
+                ,'center' //居中对齐
+                ,'right' //右对齐
+                ,'link' //超链接
+                ,'unlink' //清除链接
+                ,'face' //表情
+            ],
+            height: 150
+        }); 
+         //监听评论提交
+         form.on('submit(formSubmit)', function(data) {
+             var content = layedit.getContent(layeditNode);
+             if(typeof content == "undefined" || content == null || content == ""){
+                 return layer.alert("请输入评论内容");
+             }
+             formLoad = layer.load(1, {
+                 shade: [0.8, '#393D49']
+             });
+             $.ajax({ 
+                type: "POST",
+                url: '{{url("api/user/article_comment")}}',
+                data: {
+                    'article_id': article_id,
+                    'content': content,
+                },
+                success: function(result){
+                    layer.close(formLoad);
+                    if (result.code > 0) {
+                        layer.msg(result.msg);
+                    } else {
+                        // 刷新页面 
+                        window.location.reload();
+                    }
+                }
+            });
+             return false;
+         });
         // 文章分页
         var laypage = layui.laypage;
         laypage.render({
@@ -64,10 +123,17 @@
         });
         // 获取分页
         function ajaxGetComment(first, page = 1, limit = 10){
+            //  获取评论列表
+            ajaxLoad1 = layer.load(1, {
+                shade: [0.8, '#393D49']
+            });
             $.ajax({ 
                 type: "GET",
                 url: '{{url("api/user/article_comment")}}?article_id=' + article_id + "&page="+page+"&limit="+limit,
                 success: function(result){
+
+                    layer.close(ajaxLoad1);
+
                     if (result.code > 0) {
                         layer.msg(result.msg);
                     } else {
@@ -77,6 +143,7 @@
                             laypage.render({
                                 elem: 'article_comment_page'
                                 ,count: result.count
+                                ,height: 150
                                 ,jump: function(obj, first){
                                     var page = obj.curr?obj.curr:1;
                                     var limit = obj.limit?obj.limit:10;
