75adb223c038c107e0b9d10d77a511505a67c8d9
xiaqiuchu <27804306+xiaqiuchu@users.noreply.github.com>
2019-09-16 22:33:48 +0800

    设备日志搜索完成

--- app/Http/Controllers/Api/User/DeviceFieldLogController.php
+++ app/Http/Controllers/Api/User/DeviceFieldLogController.php
@@ -12,12 +12,41 @@ class DeviceFieldLogController extends Base
     public function index(Request $request)
     {
         $limit = $request->input('limit');
-        $deviceList = UserModel::where('token', $this->user_token())->firstOrFail()->device_field_log()->with('field_type', 'device', 'device.device_room')->orderBy('id', 'desc')->paginate($limit)->toArray();
+
+        $where = [];
+        // 字段名称
+        if($request->filled('name')){
+            $where[] = ['name', 'like', '%'.$request->name.'%'];
+        }
+        // 字段标识符
+        if($request->filled('field')){
+            $where[] = ['field', 'like', '%'.$request->field.'%'];
+        }
+        // 设备id
+        if($request->filled('device_id')){
+            $where['device_id'] = intval($request->device_id);
+        }
+
+        $deviceFieldLogList = UserModel::where('token', $this->user_token())->firstOrFail()->device_field_log()->where($where)->whereHas('device', function($query) use($request){
+            // 产品
+            if($request->filled('product_id')){
+                $query->where('product_id', intval($request->input('product_id')));
+            }
+        })->whereHas('device.device_room', function($query) use($request){
+            // 区域
+            if($request->filled('device_region_id')){
+                $query->where('device_region_id', intval($request->input('device_region_id')));
+            }
+            // 房间
+            if($request->filled('device_room_id')){
+                $query->where('device_room_id', intval($request->input('device_room_id')));
+            }
+        })->with('field_type', 'device', 'device.device_room')->orderBy('id', 'desc')->paginate($limit)->toArray();
         $returnData = [];
         $returnData['msg']              = "查询成功";
-        $returnData['count']            = $deviceList['total'];
-        $returnData['current_page']     = $deviceList['current_page'];
-        $returnData['data']             = $deviceList['data'];
+        $returnData['count']            = $deviceFieldLogList['total'];
+        $returnData['current_page']     = $deviceFieldLogList['current_page'];
+        $returnData['data']             = $deviceFieldLogList['data'];
         return success($returnData);
     }
 }
--- resources/views/user/device/device_field_log/index.blade.php
+++ resources/views/user/device/device_field_log/index.blade.php
@@ -8,12 +8,49 @@
  <body>
      <div class="layui-card">
          <div class="layui-card-body">
-            <div class="layui-row">
-                <div class="layui-btn-container">
-                    <button type="button" class="layui-btn layui-btn-sm" id="refresh-page">刷新页面</button> 
-                    <button type="button" class="layui-btn layui-btn-sm" id="refresh-product-field-region">刷新表格</button> 
+            <form class="layui-form">
+                 <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <div class="layui-btn-container">
+                            <button type="button" class="layui-btn layui-btn-sm" id="refresh-page">刷新页面</button> 
+                            <button type="button" class="layui-btn layui-btn-sm" id="refresh-product-field-region">刷新表格</button> 
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <select name="device_region_id" id="device_region_id" lay-search lay-filter="device_region_id">
+                            <option value="" selected>区域:加载中...</option>
+                        </select>
+                    </div>
+                    <div class="layui-inline">
+                        <select name="device_room_id" id="device_room_id" lay-search lay-filter="device_room_id">
+                            <option value="" selected>房间:请先选择区域.</option>
+                        </select>
+                    </div>
+                    <div class="layui-inline">
+                        <select name="product_id" id="product_id" lay-search lay-filter="product_id">
+                            <option value="" selected>产品类型:加载中...</option>
+                        </select>
+                    </div>
+                    <div class="layui-inline">
+                        <select name="device_id" id="device_id" lay-search lay-filter="device_id">
+                            <option value="" selected>设备:请先选择房间</option>
+                        </select>
+                    </div>
+                    <div class="layui-inline">
+                        <input type="text" name="name" autocomplete="off" placeholder="字段名称" class="layui-input">
+                    </div>
+                    <div class="layui-inline">
+                        <input type="text" name="field" autocomplete="off" placeholder="字段标识" class="layui-input">
+                    </div>
+                    <div class="layui-inline">
+                        <div class="layui-input-inline">
+                        <button type="submit" id="submit" class="layui-btn" lay-submit="" lay-filter="formSubmit">搜索</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
             <div class="layui-row">
                 <script type="text/html" id="bar">
                     <a class="layui-btn layui-btn-danger layui-btn-xs" lay-event="del">删除</a>
@@ -84,6 +121,109 @@
             // 刷新列表
             $('#refresh-product-field-region').click(function(){
                 table.reload('device_field_log');
+            });
+             // 获取区域分类
+             ajaxLoad3 = layer.load(1, {
+                shade: [0.8, '#393D49']
+            });
+            $.ajax({ 
+                type: "GET",
+                url: '{{url("api/user/device_region/all")}}',
+                success: function(result){
+                    layer.close(ajaxLoad3);
+                    if (result.code > 0) {
+                        layer.msg(result.msg);
+                    } else {
+                        var html='<option value="" selected>区域分类:不限区域</option>';
+                        $.each(result.data,function(key,value){
+                            html+="<option value='"+value.id+"'>"+value.name+"</option>";
+                        })
+                        $('select[name=device_region_id]').html(html);
+                        form.render("select");
+                    }
+                }
+            });
+            // 获取产品分类
+            ajaxLoad6 = layer.load(1, {
+                shade: [0.8, '#393D49']
+            });
+            $.ajax({ 
+                type: "GET",
+                url: '{{url("api/user/product/all")}}',
+                success: function(result){
+                    layer.close(ajaxLoad6);
+                    if (result.code > 0) {
+                        layer.msg(result.msg);
+                    } else {
+                        var html='<option value="" selected>产品分类:不限产品</option>';
+                        $.each(result.data,function(key,value){
+                            html+="<option value='"+value.id+"'>"+value.name+"</option>";
+                        })
+                        $('select[name=product_id]').html(html);
+                        form.render("select");
+                    }
+                }
+            });
+            // 监听区域选择
+            form.on('select(device_region_id)', function(data){
+                ajaxLoad5 = layer.load(1, {
+                    shade: [0.8, '#393D49']
+                });
+                $.ajax({ 
+                    type: "GET",
+                    url: '{{url("api/user/device_room/all")}}?device_region_id='+data.value,
+                    success: function(result){
+                        layer.close(ajaxLoad5);
+                        if (result.code > 0) {
+                            layer.msg(result.msg);
+                        } else {
+                            var html='<option value="" selected>房间:不限房间</option>';
+                            $.each(result.data,function(key,value){
+                                html+="<option value='"+value.id+"'>"+value.name+"</option>";
+                            })
+                            $('select[name=device_room_id]').html(html);
+                            form.render("select");
+                        }
+                    }
+                });
+            });   
+            // 监听房间选择
+            form.on('select(device_room_id)', function(data){
+                ajaxLoad5 = layer.load(1, {
+                    shade: [0.8, '#393D49']
+                });
+                $.ajax({ 
+                    type: "GET",
+                    url: '{{url("api/user/device/all")}}?device_room_id='+data.value,
+                    success: function(result){
+                        layer.close(ajaxLoad5);
+                        if (result.code > 0) {
+                            layer.msg(result.msg);
+                        } else {
+                            var html='<option value="" selected>设备:不限设备</option>';
+                            $.each(result.data,function(key,value){
+                                html+="<option value='"+value.id+"'>"+value.name+"</option>";
+                            })
+                            $('select[name=device_id]').html(html);
+                            form.render("select");
+                        }
+                    }
+                });
+            });   
+            //监听搜索
+            form.on('submit(formSubmit)', function(data) {
+                // 重载 table
+                table.reload('device_field_log',{
+                    where: {
+                        'device_region_id': data.field.device_region_id,
+                        'device_room_id': data.field.device_room_id,
+                        'product_id': data.field.product_id,
+                        'device_id': data.field.device_id,
+                        'field': data.field.field,
+                        'name': data.field.name,
+                    }
+                });
+                return false;
             });
      </script>
  </body>
